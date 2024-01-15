770d172615a2a00b836c6dfedf0cdb6f929132af
parameshkaramsetty <parameshkaramsetty@gmail.com>
2024-01-15 18:07:07 +0530

    learning

---
+++ C++ PROGRAMMING/Math/gcd.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<__gcd(18,8);// gives gcd of two numbers
+    /*
+    We can also get gcd of two numbers using euclidean algorithm
+    */
+}
