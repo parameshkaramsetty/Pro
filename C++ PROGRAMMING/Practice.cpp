18e72b15cc23c5f75cf46cf112423bf14698cb08
parameshkaramsetty <parameshkaramsetty@gmail.com>
2024-01-21 15:20:20 +0530

    Learning

---
+++ C++ PROGRAMMING/Practice.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void takeInput(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+}
+int main(){
+	takeInput();
+	int n;
+	cin>>n;
+	cout<<"The number is : "<<n<<"\n";
+
+
+}
