cac527ed3076e23b084ca4cadc3d36557f35ad3b
parameshkaramsetty <parameshkaramsetty@gmail.com>
2025-01-26 15:17:57 +0530

    M

--- Playground/C++/Program.cpp
+++ Playground/C++/Program.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"Success!"<<endl;
+    cout<<"Stay Consistent !!"<<endl;
 }
--- Playground/Java/Program.java
+++ Playground/Java/Program.java
@@ -3,6 +3,6 @@ class Program {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello world");
+        System.out.println("Be Consistent!");
     }
 }
