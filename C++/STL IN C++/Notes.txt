9f9bcdf83e455b47eb61c0eb4e3ac047b512f133
parameshkaramsetty <parameshkaramsetty@gmail.com>
2025-02-22 17:10:39 +0530

    M

---
+++ C++/STL IN C++/Notes.txt
@@ -0,0 +1,41 @@
+STL - Standard Template Library
+#include<bits/stdc++.h>
+using namespace std;
+
+Algorithms
+Containers
+Functions
+Iterators
+
+
+Containers
+
+    pair , vector , list , set , multiset , unordered_set , stack , queue , deque , priority_queue , map , unordered_map , multimap
+
+    Declaration
+    Data types
+    Combinations
+    Functions
+    Iterators
+
+    Pair
+    pair<int,int> p = {1,2} or pair<int,int>;
+    pair<int,string>p;
+    pair<int,vector<int>>p;
+
+
+Functions and Algorithms
+
+sort
+accumulate
+find
+erase
+count
+reverse
+comparator
+next_permuntation
+previous_permutation
+max_element
+pow
+
+
--- C++/STL IN C++/Vectors.cpp
+++ C++/STL IN C++/Vectors.cpp
@@ -2,10 +2,64 @@
 using namespace std;
 #define show(a) for(auto i : a){cout<<i<<" ";} cout<<endl;
 #define nl cout<<endl;
+void vector_size(){
+    // cout<<v.size();
+}
+void vector_sum(){
+
+}
+void vector_iterator(){
+    vector<int>v = {1,2,3};
+    vector<int>::iterator it = v.begin();//gives the adddress
+    v.begin();//
+    v.end();// address next to the last element
+    cout<<*it<<endl;// pointer to the first element
+
+}
+
 int main(){    
-    vector<int>v(5);
-    for(int i=0;i<5;i++){
-        cin>>v[i];
-    }
-    show(v);
+
+    // Declaration
+
+
+    // Input - 2 Types
+
+    // Inorder to utilize based on index then declare the vector size at the time of initialization
+    // vector<int>v(5)
+    // then we can take input directly to the vector using for loop 0 to 5 v[i]
+
+    // vector<int>v
+    // take value in element and push it into vector v.push_back(p)
+
+    // Vector Of Pairs
+    // vector<pair<int,int>>v;
+    // v.push_back(first,second);
+
+    // when size is not declared
+    // vector<pair<int,int>>v;
+    // // v.push_back({first,second});
+    // v.push_back({2,3});
+    // v.push_back({4,5});
+    // for(auto i : v){
+    // cout<<i.first<<" "<<i.second<<endl;
+    // }
+
+    // when size is declared
+    // cin>>v[i].first>>v[i].second;
+
+    // size declared => v[i];
+
+    // vector<int> v = {1,2,3,4,5}; // values cannot be given when size is given
+    // vector<int>v(5) // takes garbage value
+
+    // size of vector - vector_name.size();
+
+    vector<int>a = {7,100,45,18,8};
+
+    cout<<a.front()<<endl;
+    auto it = find(begin(a),end(a),2)-begin(a);
+    cout<<it != end(a)<<endl;
+
+
+
 }
