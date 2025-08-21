ed44d637c1dea02d625232a0963fee3fadcd26d9
parameshkaramsetty <parameshkaramsetty@gmail.com>
2025-08-21 23:37:25 +0530

    added new files

--- Playground/C++/Program.cpp
+++ Playground/C++/Program.cpp
@@ -1,13 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-void learn_pair(){
-    pair<int,int>p = {1,2};
-    cout<<p.first<<" "<<p.second<<endl;
-
-    pair<int,int>mp = make_pair(3,4);
-    cout<<mp.first<<" "<<mp.second<<endl;
-}
 int main(){
-
-    learn_pair();
+    vector<int>a = {1,2,3,4,5,6,7};
+    int n = a.size();
+    cout<<"size = "<<n<<endl;
+    int target = 1;
+    int s =  0,e = n-1;
+    while(s<=e){
+        int mid = (s + (e-s))/2;
+        if(a[mid]==target){
+            cout<<mid<<endl;
+            break;
+        }
+        else if(a[mid]>target){
+            e=mid-1;
+        }
+        else{
+            s = mid+1;
+        }
+    }
 }
--- Playground/Python/program.py
+++ Playground/Python/program.py
@@ -1,1 +1,6 @@
-print(input())
+def rev(n):
+    if(n==0):return 
+    print(n)
+    rev(n-1)
+n=int(input())
+rev(n)
